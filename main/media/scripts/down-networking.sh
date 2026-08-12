82e0acec4d5b18cb44274ba1eaac1fe5eb827da0
ggjorven <jorbenvanderwal@gmail.com>
2026-08-12 15:05:01 +0200

    Fix down-networking scripts not actually shutting down the network.

--- main/media/scripts/down-networking.sh
+++ main/media/scripts/down-networking.sh
@@ -2,4 +2,4 @@
 
 # Networking
 cd /home/media/networking
-docker compose --env-file ../.env --env-file .env down
+docker compose --all-resources --env-file ../.env --env-file .env down
--- main/priv-net/scripts/down-networking.sh
+++ main/priv-net/scripts/down-networking.sh
@@ -2,4 +2,4 @@
 
 # Networking
 cd /home/privnet/networking
-docker compose --env-file ../.env --env-file .env down
+docker compose --all-resources --env-file ../.env --env-file .env down
