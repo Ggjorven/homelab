73e511b8a2a65a7dec52728917b4b7fae05cf817
ggjorven <jorbenvanderwal@gmail.com>
2026-08-12 19:54:02 +0200

    Remove unnecessary restart from vpn-restarts script in arr.

--- main/arr/scripts/vpns-restart.sh
+++ main/arr/scripts/vpns-restart.sh
@@ -9,13 +9,9 @@ restart_if_running() {
 # VPN1
 restart_if_running vpn1
 restart_if_running byparr
-restart_if_running qbittorrent
 restart_if_running jackett
 restart_if_running prowlarr
-restart_if_running radarr
-restart_if_running sonarr
-restart_if_running lidarr
-restart_if_running bazarr
+restart_if_running qbittorrent
 
 # VPN2
 restart_if_running vpn2
